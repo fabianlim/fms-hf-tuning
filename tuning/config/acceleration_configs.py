105a9bf1d16ed9c8a05da5cb552d62213937162d
Yu Chin Fabian Lim <flim@sg.ibm.com>
2024-06-09 10:05:25 +0000

    added comments on design
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>

--- tuning/config/acceleration_configs.py
+++ tuning/config/acceleration_configs.py
@@ -3,6 +3,23 @@ from typing import Annotated, List, Dict, Type
 from tuning.utils.import_utils import is_fms_accelerate_available
 import yaml
 
+# DESIGN OF FMS CONFIGS:
+# - FMS will have differnt configs (probably one (or more) / plugin).
+# - e,g. QuantizedLoraConfig will be for the accelerated_peft plugin
+# - e.g, FusedOpsAndKernelsConfig will be for fused_ops_and_kernels plugin
+# - FMS users will understand that to use thse configs, they will need
+#   to install the plugin that corresponds to that config
+# - each FMS config will nest multiple dataclasses in a single level
+# - typically each nested dataclass corresponds to one use case
+# - e.g. for the QuantizedLoraConfig, two use cases of auto_gptq and bnb_qlora
+
+# - the HF dataclass argument parser will create position arguments from the
+#   FMS config
+# - in the usal way, the keys of the FMS config will correspond to a --key
+# - then the use case dataclass will be passed its attributes by position
+# - hence, this is the reason why we enforce the FMS config to be
+#   single-level nested dataclasses.
+
 @dataclass
 class QuantizedLoraConfig:
 
@@ -50,10 +67,25 @@ class BNBQLoraConfig:
             raise ValueError("quant_type can only be either 'nf4' or 'fp4.")
 
 if is_fms_accelerate_available():
+
+    # DESIGN OF ACCELERATION CONFIGS
+    # - An ACCELERATION CONFIG is a monolothic config passed to AccelerationFramework
+    # - it is NOT meant to be user facing. Users will only configure
+    #   use case dataclasses within.
+    # - however, uses can consult the annotations (see below) to understand
+    #   which use-case config can be active at the same time.
+    # - it is a collection of use-case dataclasses (see above)
+    # - every use-case dataclass is annotated with a header
+    # - any two use-case dataclasses that are annotated with the 
+    #   same header, cannot be active at the same time.
+    # - An Acceleration Config is valid only if it does not have any 
+    #   use-case dataclass that violates these rules.
     from fms_acceleration.constants import KEY_PLUGINS
 
-    # helper function to parse
     def parse_acceleration_config(config: "AccelerationConfig"):
+        """convert a valid AcceleratoinConfig dataclass into a schema-less dictionary
+        as dictated by the header annotations.
+        """
 
         # populate a dictionary
         configuration_contents = {}
@@ -85,27 +117,26 @@ if is_fms_accelerate_available():
 
         return configuration_contents
 
-    # this is a demostration how to parse the AccelerationConfig into a yaml
     def parse_acceleration_config_to_yaml(config: "AccelerationConfig", filename: str):
+        "convert a valid AccelerationConfig dataclass into a yaml"
         configuration_contents = parse_acceleration_config(config)
         with open(filename, "w") as f:
             yaml.dump({KEY_PLUGINS: configuration_contents}, f)
 
-
-    # this should be passed to AccelerationFramework
     @dataclass
     class AccelerationConfig:
+        "Dataclass that manages configuration of AccelerationFramework"
 
         # each field will a single-level dataclass
         auto_gptq: Annotated[AutoGPTQLoraConfig, "peft", "quantization"] = None
 
         bitsandbytes: Annotated[BNBQLoraConfig, "peft", "quantization"] = None
 
-    # this is a demonstration of how to parse daclasses into acceleration config
     def convert_dataclasses_to_acceleration_config(*dataclasses: Type):
+        "Convert one or many FMS configs to a monolithic AccelerationConfig"
+
         config = AccelerationConfig()
         rem_fields = {fi.name: fi for fi in fields(config)} # these need to be parsed
-        # rem_fields = {fields(config)} # these need to be parsed
 
         def _convert(*dcs: Type):
             for dc in dcs:
